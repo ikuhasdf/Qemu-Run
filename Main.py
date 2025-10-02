11d575f48653dc8eac68055eceedf394592abbba
你这东西保熟吗，小学生的我写出来的肯定了 <ikuhasdf@outlook.com>
2025-10-02 16:59:38 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -6,13 +6,13 @@ class MainUI:
     def __init__(self):
         self.vm_cmd = ["qemu-system-x86_64"]
         self.root = Tk()
-        self.root.title("Qemu启动器1.0-Qemu 4.1.0")
-        self.root.geometry("800x600")
+        self.root.title("Qemu启动器1.0")
+        self.root.geometry("300x900")
         #虚拟机名字
         def vmruncmd():
             self.command_str = " ".join(self.vm_cmd)
             messagebox.showerror("命令", self.command_str)
-        self.vm_cmd_cmd = Button(self.root,text="虚拟机命令",command=vmruncmd)
+        self.vm_cmd_cmd = ttk.Button(self.root,text="虚拟机命令",command=vmruncmd)
         self.vm_cmd_cmd.pack()
         self.vm_name_title = Label(self.root,text="输入虚拟机名字")
         self.vm_name_title.pack()
@@ -93,6 +93,26 @@ class MainUI:
             self.vm_cmd.extend(["-smp", self.vm_core])
         self.vm_core_button = ttk.Button(self.root,text="确定",command=addcore)
         self.vm_core_button.pack()
+        #虚拟机声卡
+        self.vm_sound_title = Label(self.root,text="输入声卡型号")
+        self.vm_sound_title.pack()
+        self.vm_sound_1 = Entry(self.root)
+        self.vm_sound_1.pack()
+        def addsound():
+            self.vm_sound = self.vm_sound_1.get()
+            self.vm_sound.extend(["-device", self.vm_sound])
+        self.vm_sound_button = ttk.Button(self.root,text="确定",command=addsound)
+        self.vm_sound_button.pack()
+        #虚拟机网卡
+        self.vm_net_title = Label(self.root,text="输入网卡型号")
+        self.vm_net_title.pack()
+        self.vm_net_1 = Entry(self.root)
+        self.vm_net_1.pack()
+        def addnet():
+            self.vm_net = self.vm_net_1.get()
+            self.vm_net.extend(["-device", self.vm_net])
+        self.vm_net_button = ttk.Button(self.root,text="确定",command=addnet)
+        self.vm_net_button.pack()
         #虚拟机共享文件夹
         self.vm_vvfat_title = Label(self.root,text="输入共享文件夹路径")
         self.vm_vvfat_title.pack()
@@ -117,7 +137,7 @@ class MainUI:
             entries = [
                 self.vm_name_1, self.vm_class_1, self.vm_kvm_1,
                 self.vm_cpu_1, self.vm_gpu_1, self.vm_hd_1,
-                self.vm_vvfat_1, self.vm_ram_1, self.vm_core_1
+                self.vm_vvfat_1, self.vm_ram_1, self.vm_core_1,self.vm_sound_1,self.vm_net_1
             ]
         self.clear_button = ttk.Button(self.root,text="清空",command=clear_config).pack()
         self.root.mainloop()
