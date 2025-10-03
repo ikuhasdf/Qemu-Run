34c34b2fc2978ee117269c1e72ace83fad9da88e
你这东西保熟吗，小学生的我写出来的肯定了 <ikuhasdf@outlook.com>
2025-10-03 23:04:02 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,141 +4,206 @@ from tkinter import ttk,messagebox
 import threading
 class MainUI:
     def __init__(self):
+        #初始化
         self.vm_cmd = ["qemu-system-x86_64"]
         self.root = Tk()
-        self.root.title("Qemu启动器1.0")
-        self.root.geometry("300x900")
+        self.root.title("Qemu启动器1.1")
+        self.root.geometry("260x480")
+        scrollbar = Scrollbar(self.root)
+        scrollbar.pack(side="right", fill="y")
+        canvas = Canvas(self.root, yscrollcommand=scrollbar.set)
+        canvas.pack(side="left", fill="both", expand=True)
+        scrollbar.config(command=canvas.yview)
+        self.main_frame = Frame(canvas)
+        canvas.create_window((0, 0), window=self.main_frame, anchor="nw")
+        def configure_canvas(event):
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+        self.main_frame.bind("<Configure>", configure_canvas)
+
+        def on_mousewheel(event):
+            canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+        canvas.bind("<MouseWheel>", on_mousewheel)
+
         #虚拟机名字
         def vmruncmd():
             self.command_str = " ".join(self.vm_cmd)
-            messagebox.showerror("命令", self.command_str)
-        self.vm_cmd_cmd = ttk.Button(self.root,text="虚拟机命令",command=vmruncmd)
+            messagebox.showinfo("命令", self.command_str)
+        self.vm_cmd_cmd = ttk.Button(self.main_frame,text="虚拟机命令",command=vmruncmd)
         self.vm_cmd_cmd.pack()
-        self.vm_name_title = Label(self.root,text="输入虚拟机名字")
+        self.vm_name_title = Label(self.main_frame,text="输入虚拟机名字")
         self.vm_name_title.pack()
-        self.vm_name_1 = Entry(self.root)
+        self.vm_name_1 = ttk.Entry(self.main_frame)
         self.vm_name_1.pack()
         def addname():
             self.vm_name = self.vm_name_1.get()
             self.vm_cmd.extend(["-name", self.vm_name])
-        self.vm_name_button = ttk.Button(self.root,text="确定",command=addname)
+        self.vm_name_button = ttk.Button(self.main_frame,text="确定",command=addname)
         self.vm_name_button.pack()
         #虚拟机类型
-        self.vm_class_title = Label(self.root,text="输入虚拟机类型")
+        self.vm_class_title = Label(self.main_frame,text="输入虚拟机类型")
         self.vm_class_title.pack()
-        self.vm_class_1 = Entry(self.root)
+        self.vm_class_1 = ttk.Entry(self.main_frame)
         self.vm_class_1.pack()
         def addclass():
             self.vm_class = self.vm_class_1.get()
             self.vm_cmd.extend(["-M", self.vm_class])
-        self.vm_class_button = ttk.Button(self.root,text="确定",command=addclass)
+        self.vm_class_button = ttk.Button(self.main_frame,text="确定",command=addclass)
         self.vm_class_button.pack()
         #虚拟机加速
-        self.vm_kvm_title = Label(self.root,text="输入虚拟机加速")
+        self.vm_kvm_title = Label(self.main_frame,text="输入虚拟机加速")
         self.vm_kvm_title.pack()
-        self.vm_kvm_1 = Entry(self.root)
+        self.vm_kvm_1 = ttk.Entry(self.main_frame)
         self.vm_kvm_1.pack()
         def addkvm():
             self.vm_kvm = self.vm_kvm_1.get()
             self.vm_cmd.extend(["-accel", self.vm_kvm])
-        self.vm_kvm_button = ttk.Button(self.root,text="确定",command=addkvm)
+        self.vm_kvm_button = ttk.Button(self.main_frame,text="确定",command=addkvm)
         self.vm_kvm_button.pack()
         #虚拟机CPU
-        self.vm_cpu_title = Label(self.root,text="输入CPU型号")
+        self.vm_cpu_title = Label(self.main_frame,text="输入CPU型号")
         self.vm_cpu_title.pack()
-        self.vm_cpu_1 = Entry(self.root)
+        self.vm_cpu_1 = ttk.Entry(self.main_frame)
         self.vm_cpu_1.pack()
         def addcpu():
             self.vm_cpu = self.vm_cpu_1.get()
             self.vm_cmd.extend(["-cpu", self.vm_cpu])
-        self.vm_cpu_button = ttk.Button(self.root,text="确定",command=addcpu)
+        self.vm_cpu_button = ttk.Button(self.main_frame,text="确定",command=addcpu)
         self.vm_cpu_button.pack()
         #虚拟机显卡
-        self.vm_gpu_title = Label(self.root,text="输入显卡型号")
+        self.vm_gpu_title = Label(self.main_frame,text="输入显卡型号")
         self.vm_gpu_title.pack()
-        self.vm_gpu_1 = Entry(self.root)
+        self.vm_gpu_1 = ttk.Entry(self.main_frame)
         self.vm_gpu_1.pack()
         def addgpu():
             self.vm_gpu = self.vm_gpu_1.get()
             self.vm_cmd.extend(["-vga", self.vm_gpu])
-        self.vm_gpu_button = ttk.Button(self.root,text="确定",command=addgpu)
+        self.vm_gpu_button = ttk.Button(self.main_frame,text="确定",command=addgpu)
         self.vm_gpu_button.pack()
         #虚拟机磁盘
-        self.vm_hd_title = Label(self.root,text="输入磁盘路径")
+        self.vm_hd_title = Label(self.main_frame,text="输入磁盘路径")
         self.vm_hd_title.pack()
-        self.vm_hd_1 = Entry(self.root)
+        self.vm_hd_1 = ttk.Entry(self.main_frame)
         self.vm_hd_1.pack()
         def addhd():
             self.vm_hd = self.vm_hd_1.get()
             self.vm_cmd.extend(["-hda", self.vm_hd])
-        self.vm_hd_button = ttk.Button(self.root,text="确定",command=addhd)
+        self.vm_hd_button = ttk.Button(self.main_frame,text="确定",command=addhd)
         self.vm_hd_button.pack()
         #虚拟机内存
-        self.vm_ram_title = Label(self.root,text="输入内存容量MB")
+        self.vm_ram_title = Label(self.main_frame,text="输入内存容量MB")
         self.vm_ram_title.pack()
-        self.vm_ram_1 = Entry(self.root)
+        self.vm_ram_1 = ttk.Entry(self.main_frame)
         self.vm_ram_1.pack()
         def addram():
             self.vm_ram = self.vm_ram_1.get()
             self.vm_cmd.extend(["-m", self.vm_ram])
-        self.vm_ram_button = ttk.Button(self.root,text="确定",command=addram)
+        self.vm_ram_button = ttk.Button(self.main_frame,text="确定",command=addram)
         self.vm_ram_button.pack()
         #虚拟机核心
-        self.vm_core_title = Label(self.root,text="输入CPU核心")
+        self.vm_core_title = Label(self.main_frame,text="输入CPU核心")
         self.vm_core_title.pack()
-        self.vm_core_1 = Entry(self.root)
+        self.vm_core_1 = ttk.Entry(self.main_frame)
         self.vm_core_1.pack()
         def addcore():
             self.vm_core = self.vm_core_1.get()
             self.vm_cmd.extend(["-smp", self.vm_core])
-        self.vm_core_button = ttk.Button(self.root,text="确定",command=addcore)
+        self.vm_core_button = ttk.Button(self.main_frame,text="确定",command=addcore)
         self.vm_core_button.pack()
         #虚拟机声卡
-        self.vm_sound_title = Label(self.root,text="输入声卡型号")
+        self.vm_sound_title = Label(self.main_frame,text="输入声卡型号")
         self.vm_sound_title.pack()
-        self.vm_sound_1 = Entry(self.root)
+        self.vm_sound_1 = ttk.Entry(self.main_frame)
         self.vm_sound_1.pack()
         def addsound():
             self.vm_sound = self.vm_sound_1.get()
             self.vm_cmd.extend(["-device", self.vm_sound])
-        self.vm_sound_button = ttk.Button(self.root,text="确定",command=addsound)
+        self.vm_sound_button = ttk.Button(self.main_frame,text="确定",command=addsound)
         self.vm_sound_button.pack()
         #虚拟机网卡
-        self.vm_net_title = Label(self.root,text="输入网卡型号")
+        self.vm_net_title = Label(self.main_frame,text="输入网卡型号")
         self.vm_net_title.pack()
-        self.vm_net_1 = Entry(self.root)
+        self.vm_net_1 = ttk.Entry(self.main_frame)
         self.vm_net_1.pack()
         def addnet():
             self.vm_net = self.vm_net_1.get()
             self.vm_cmd.extend(["-device", self.vm_net])
-        self.vm_net_button = ttk.Button(self.root,text="确定",command=addnet)
+        self.vm_net_button = ttk.Button(self.main_frame,text="确定",command=addnet)
         self.vm_net_button.pack()
         #虚拟机共享文件夹
-        self.vm_vvfat_title = Label(self.root,text="输入共享文件夹路径")
+        self.vm_vvfat_title = Label(self.main_frame,text="输入共享文件夹路径")
         self.vm_vvfat_title.pack()
-        self.vm_vvfat_1 = Entry(self.root)
+        self.vm_vvfat_1 = ttk.Entry(self.main_frame)
         self.vm_vvfat_1.pack()
         def addvvfat():
             self.vm_vvfat = self.vm_vvfat_1.get()
             self.vm_cmd.extend(["-drive", f"format=vvfat,dir={self.vm_vvfat},rw=on"])
-        self.vm_vvfat_button = ttk.Button(self.root,text="确定",command=addvvfat)
+        self.vm_vvfat_button = ttk.Button(self.main_frame,text="确定",command=addvvfat)
         self.vm_vvfat_button.pack()
+        #虚拟机光盘
+        self.vm_cd_title = Label(self.main_frame,text="输入光盘路径")
+        self.vm_cd_title.pack()
+        self.vm_cd_1 = ttk.Entry(self.main_frame)
+        self.vm_cd_1.pack()
+        def addcd():
+            self.vm_cd = self.vm_cd_1.get()
+            self.vm_cmd.extend(["-cdrom", self.vm_cd])
+        self.vm_cd_button = ttk.Button(self.main_frame,text="确定",command=addcd)
+        self.vm_cd_button.pack()
+        #虚拟机软盘
+        self.vm_f_title = Label(self.main_frame,text="输入软盘路径")
+        self.vm_f_title.pack()
+        self.vm_f_1 = ttk.Entry(self.main_frame)
+        self.vm_f_1.pack()
+        def addf():
+            self.vm_f = self.vm_f_1.get()
+            self.vm_cmd.extend(["-fda", self.vm_f])
+        self.vm_f_button = ttk.Button(self.main_frame,text="确定",command=addf)
+        self.vm_f_button.pack()
+        #虚拟机启动选项
+        self.vm_boot_title = Label(self.main_frame,text="输入虚拟机启动选项,硬盘选C，软盘选A，光盘选择D")
+        self.vm_boot_title.pack()
+        self.vm_boot_1 = ttk.Entry(self.main_frame)
+        self.vm_boot_1.pack()
+        def addboot():
+            self.vm_boot = self.vm_boot_1.get()
+            self.vm_cmd.extend(["-boot", self.vm_boot])
+        self.vm_boot_button = ttk.Button(self.main_frame,text="确定",command=addboot)
+        self.vm_boot_button.pack()
+        #虚拟机高级参数
+        self.vm_tall_title = Label(self.main_frame,text="输入额外QEMU参数")
+        self.vm_tall_title.pack()
+        self.vm_tall_1 = ttk.Entry(self.main_frame)
+        self.vm_tall_1.pack()
+        def addtall():
+            self.vm_tall = self.vm_tall_1.get()
+            self.vm_cmd.extend([self.vm_tall])
+        self.vm_tall_button = ttk.Button(self.main_frame,text="确定",command=addtall)
+        self.vm_tall_button.pack()
         #虚拟机启动
         def vmrun1():
             run(self.vm_cmd)
         def vmrun():
             thread = threading.Thread(target=vmrun1, daemon=True)
             thread.start()
-        self.vmrunbutton = ttk.Button(self.root,text="启动",command=vmrun)
+        self.vmrunbutton = ttk.Button(self.main_frame,text="启动",command=vmrun)
         self.vmrunbutton.pack()
         #清空按钮
         def clear_config():
             self.vm_cmd = ["qemu-system-x86_64"]
+        self.clear_button = ttk.Button(self.main_frame,text="清空QEMU指令",command=clear_config).pack()
+        #清空所有参数按钮
+        def clear_config1():
+            self.vm_cmd = ["qemu-system-x86_64"]
             entries = [
                 self.vm_name_1, self.vm_class_1, self.vm_kvm_1,
                 self.vm_cpu_1, self.vm_gpu_1, self.vm_hd_1,
-                self.vm_vvfat_1, self.vm_ram_1, self.vm_core_1,self.vm_sound_1,self.vm_net_1
+                self.vm_vvfat_1, self.vm_ram_1, self.vm_core_1,self.vm_sound_1,self.vm_net_1,
+                self.vm_cd_1,self.vm_tall_1,self.vm_f_1,self.vm_boot_1
             ]
-        self.clear_button = ttk.Button(self.root,text="清空",command=clear_config).pack()
+            for entry in entries:
+                entry.delete(0, END)
+        self.clear_button_1 = ttk.Button(self.main_frame,text="清空所有参数",command=clear_config1).pack()
         self.root.mainloop()
 MainUI()
